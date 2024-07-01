1aabc104afa47877ac65e61e86dc1fcc1509c402
Prabhashana Muthukumarana <mudithaprabhashana@gmail.com>
2024-07-01 11:31:21 +0530

    first commit -commiting all files to the repository

---
+++ dropdown.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <a href="" class="navbar-brand">Gold insurance </a>
+            </div>
+
+            <div id="navbar" class="navbar-collapse collapse">
+               <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+
+                    <!-- Hospitalization dropdown menu -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hospitalization <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Government Hospitalization</a></li>
+                            <li><a href="#">Government Ayurvedic Hospitalization</a></li>
+                            <li><a href="#">Private Hospitalization</a></li>
+                            <li><a href="#">Private Ayurvedic Hospitalization</a></li>
+                            <li><a href="#">Heart Surgery - Dependent</a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Child Birth * 2<b class="caret"></b></a>
+                    </li>
+                    
+
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Heart<b class="caret"></b></a>
+                    </li>
+                   
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Cancer<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Kidney<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Brain<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Knee<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Hip<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Hearing Aid<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Spectacles<b class="caret"></b></a>
+                    </li>
+
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Death<b class="caret"></b></a>
+                    </li>
+                    
+                    <li>
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Accident<b class="caret"></b></a>
+                    </li>
+
+                   
+                </ul>
+            </div>
+
+        </nav>
+    </div>
+    
+</body>
+</html>
